f8aac5352690a15e7dce557df8b837d1db4ff305
Jankie <qwq@awaish.link>
2024-02-23 00:44:37 +0800

    [feat] multiprocessing

--- src/client.py
+++ src/client.py
@@ -1,5 +1,5 @@
 import socket
-import threading
+import multiprocessing
 import random
 
 def send_request():
@@ -8,18 +8,18 @@ def send_request():
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             sock.connect(('img.alicdn.com', 80))
-            request = b'GET /imgextra/i1/O1CN01xA4P9S1JsW2WEg0e1_!!6000000001084-2-tps-2880-560.png?' + str(random.random()).encode() + b' HTTP/1.1\r\nHost: img.alicdn.com\r\nConnection: close\r\n\r\n'
-            sock.sendall(request)
+            request = 'GET /imgextra/i1/O1CN01xA4P9S1JsW2WEg0e1_!!6000000001084-2-tps-2880-560.png?{} HTTP/1.1\r\nHost: img.alicdn.com\r\nConnection: close\r\n\r\n'.format(random.random())
+            sock.sendall(request.encode())
             response = sock.recv(1024)
             sock.close()
         except:
             send_request()
 
 def main():
-    for i in range(1024):
+    for i in range(128):
         try:
-            thread = threading.Thread(target=send_request)
-            thread.start()
+            process = multiprocessing.Process(target=send_request)
+            process.start()
         except Exception as e:
             print(e)
 
