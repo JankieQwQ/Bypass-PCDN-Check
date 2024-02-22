973534e984ce23b56954b663a7535fad630d3fde
JankieQwQ <qwq@awaish.link>
2024-02-23 00:17:45 +0800

    Update client.py

--- src/client.py
+++ src/client.py
@@ -13,9 +13,21 @@ def main():
     ip_list = nslookup(domain)
     size = random.randint(1,10)
     for i in range(1024):
-        for host in ip_list:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((host, 80))
-            data = b'0' * size * 1024 * 1024 
-            sock.sendall(data)
-            sock.close()
+        try:
+            for host in ip_list:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((host, 80))
+                data = b'0' * size * 1024 * 1024 
+                sock.sendall(data)
+                sock.close()
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    print('PCDN CHECK BYPASSER')
+    print('Use Ctrl-C Return to exit.')
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        pass
