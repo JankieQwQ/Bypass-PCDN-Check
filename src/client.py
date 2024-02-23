90671eab412450bdbe28119227fb0dc8e46fb9b3
Jankie <qwq@awaish.link>
2024-02-23 12:58:57 +0800

    [style] Object Oriented Programming: Request

--- src/client.py
+++ src/client.py
@@ -2,26 +2,30 @@ import socket
 import multiprocessing
 import random
 
-def send_request():
-    while True:
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            sock.connect(('img.alicdn.com', 80))
-            request = 'GET /imgextra/i1/O1CN01xA4P9S1JsW2WEg0e1_!!6000000001084-2-tps-2880-560.png?{} HTTP/1.1\r\nHost: img.alicdn.com\r\nConnection: close\r\n\r\n'.format(random.random())
-            sock.sendall(request.encode())
-            response = sock.recv(1024)
-            sock.close()
-        except:
-            send_request()
+class request:
+    def __init__(self,processingNumber:int) -> None:
+        self.processingNumber = processingNumber
 
-def main(processingNumber:int):
-    for i in range(processingNumber):
-        try:
-            process = multiprocessing.Process(target=send_request)
-            process.start()
-        except Exception as e:
-            print(e)
+    def send_request():
+        while True:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                sock.connect(('img.alicdn.com', 80))
+                request = 'GET /imgextra/i1/O1CN01xA4P9S1JsW2WEg0e1_!!6000000001084-2-tps-2880-560.png?{} HTTP/1.1\r\nHost: img.alicdn.com\r\nConnection: close\r\n\r\n'.format(random.random())
+                sock.sendall(request.encode())
+                response = sock.recv(1024)
+                sock.close()
+            except:
+                send_request()
+
+    def main(self):
+        for i in range(self.processingNumber):
+            try:
+                process = multiprocessing.Process(target=send_request)
+                process.start()
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
     print('PCDN CHECK BYPASSER')
